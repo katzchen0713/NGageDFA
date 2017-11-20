15a8e9d9f76d0b96e161a8ead525341b8ccb7502
Kate <katzchen0713@gmail.com>
2017-11-19 21:23:24 -0500

    initial

---
+++ app/src/main/java/dfa/ngage/MainActivity.java
@@ -0,0 +1,13 @@
+package dfa.ngage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
